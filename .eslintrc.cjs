5d1fdedeac795272c87579cf19d1767ff720072a
Allen Tao <taoqingqiu@gmail.com>
2023-02-11 23:02:48 +0800

    fix(README.md): handle conflicts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["standard-with-typescript"],
-  overrides: [],
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  rules: {},
-};
+  rules: {
+  }
+}
